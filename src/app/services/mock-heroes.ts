f877f0c61f6e1d96707f0af24c9d2e9ea49641b5
Daniel Santos <daniel.hoshiaki@gmail.com>
2018-04-30 21:29:39 -0300

    Daniel 0.2

---
+++ src/app/services/mock-heroes.ts
@@ -0,0 +1,12 @@
+
+import { Hero } from "../model/heroes";
+
+export const HEROES : Hero[] = [
+    { id: 11, name: 'Supermain'},
+    { id: 12, name: 'Spiderman'},
+    { id: 13, name: 'Flash'},
+    { id: 14, name: 'Batman'},
+    { id: 15, name: 'Homem de Ferro'},
+    { id: 16, name: 'Mulher Maravilha'},
+    { id: 17, name: 'Lanterna Verde'},
+]
